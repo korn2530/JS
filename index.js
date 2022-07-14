00a1def695bad121fa517bb40cbf18533c8be833
korn2530 <jhona25_10@hotmail.com>
2022-07-14 01:58:03 -0500

    JS encapsulation

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 
 
-//un objeto no puede ser independiente de otro objeto
+//(Encapsulamiento)Simplifica el uso del objeto, sin que el usuario sepa que hay en el interior.
+/*
+const company = {
+         
+    name: "Rocketseat",
+    employes:[],
 
-const person = {
-         name: 'John',
-         locaname : 'London',
+    sortEmployes: function(){
 
-         addres:{
-                street: '123 Main Street',
-                city: 'London',
-                country: 'UK'
-         }
+    }
 
 }
 
-addres:{
-    street: '123 Main Street',
-    city: 'London',
-    country: 'UK'
+
+//a la propiedad sortemployees lo convierto en string 
+company.sortEmployes = "adadasdas"
+
+company.sortEmployes()
+*/
+
+
+function Company(name){
+
+    let employes = [];
+     this.name = name;
+
+     this.getEmployes = function(){
+         return employes;
+     }
+
+     this.addEmployes = function(employe) {
+         employes.push(employe);
+     }
+
 }
+
+const company = new Company("Rocketseat");
+const company2 = new Company("pepsi");
+
+console.log(company);
+console.log(company2);
+
+company.addEmployes({name: "Diego"});
+company2.addEmployes({name: "Diego"});
+
+
+console.log(company.getEmployes());
+console.log(company2.getEmployes());
+
+
+
